3da12bc0617e2344032e0eeed8ba4dd6d118cfac
JAVIER32456 <ronaltomas932@gmail.com>
2024-08-31 10:29:31 -0500

    URL

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://javier32456.github.io/DashboardInstitucion",
+  base: "https://javier32456.github.io/FormularioHorasExtras",
 })
 
